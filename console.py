7001aaec4d0268b255593115a0692986265c7b19
Sombreroboy <83668223+Sombreroboy@users.noreply.github.com>
2021-05-29 11:55:57 -0600

    Player and console should be finished now

--- console.py
+++ console.py
@@ -1,9 +1,20 @@
-class CONSOLE():
+from game.board import Board
+from game.combination import Combination
+from game.player import Player
+
+class Console():
+    def __init__(self):
+        """The class constructor.
+        """
+        self.player = Player()
+        self.console = Console()
+        self.board = Board()
+
     def print_dashes():
-    #prints a line of dashes?
+    #prints a line of dashes for displaly
         print()
-        print('___________________________')
+        print('_________________________________')
         
-    def print_players(dict):
+    def print_info(self):
     #prints out a name, their guess, and the hint
-        return
+        print("Player, " + self.Player.name + "has guessed " + self.Player.guess + ". Their hint is, " + self.Board.get_hint + ".")
--- player.py
+++ player.py
@@ -1,32 +1,28 @@
-"""
-PLAYER()
-   
-   get_name:
-      input name statement
-      return name
-   start_turn(name):
-      print "Player {name}'s turn"
-   get_guess():
-      return response to "what is your guess?"
-      """
 class Player():
     def __init__(self):
         self.name = "Player1"
-        self.names = []
+        #(unused code)self.names = []
         self.guesses = []
 
     def get_name(self):
         #Print previous guesses?
-        name = str(input("Guess your number 1000-9999! "))
-        while len(guess) != 1 or guess in self.guesses:
-            guess = input("Guess your number 1000-9999! ")
+        name = str(input("Please enter a name for your player: "))
         self.name = name
-        self.names.append(name)
-        return name
+        #(unused code)self.names.append(name)
+        #return name
     def get_guess(self):
         #Print previous guesses?
+        print(self.name + "'s turn!")
         guess = input("Guess your number 1000-9999! ")
         while guess <1000 or guess >9999 or guess in self.guesses:
-            guess = input("Guess your number 1000-9999! ")
+            if guess < 1000:
+                print("To small, enter a 4 digit number please.")
+                guess = input("Guess your number 1000-9999! ")
+            elif guess > 9999:
+                print("To large, enter a 4 digit number please.")
+                guess = input("Guess your number 1000-9999! ")
+            elif guess in self.guesses:
+                print("You already guessed that!")
+                guess = input("Guess your number 1000-9999! ")
         self.guesses.append(guess)  
         return guess
